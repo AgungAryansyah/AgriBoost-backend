3b3852567f060c106ef37b0038071c834fa97e7d
agungaryansyah <agungaryansyah2006@gmail.com>
2025-02-27 07:56:47 +0700

    chore: adding a few things to git

---
+++ internal/infra/jwt/jwt.go
@@ -0,0 +1,57 @@
+package jwt
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+)
+
+type JWTItf interface {
+	GenerateToken(id uuid.UUID) (string, error)
+}
+
+type JWT struct {
+	secretKey string
+	expiresAt time.Time
+}
+
+func NewJwt() JWTItf {
+	err := godotenv.Load()
+	if err != nil {
+		return nil
+	}
+	exp, err := strconv.Atoi(os.Getenv("JWT_EXPIRED"))
+	if err != nil {
+		return nil
+	}
+	secret := os.Getenv("JWT_SECRET")
+	return &JWT{
+		secretKey: secret,
+		expiresAt: time.Now().Add(time.Duration(exp) * time.Hour),
+	}
+}
+
+type Claims struct {
+	Id uuid.UUID
+	jwt.RegisteredClaims
+}
+
+func (j *JWT) GenerateToken(id uuid.UUID) (string, error) {
+	claim := Claims{
+		Id: id,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(j.expiresAt),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	tokenString, err := token.SignedString(j.secretKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
