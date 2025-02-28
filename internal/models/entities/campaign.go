795ac0e3974573746af1bfbcb992cb624428c636
agungaryansyah <agungaryansyah2006@gmail.com>
2025-02-28 22:30:01 +0700

    fix: change some status data type to bool

--- internal/models/entities/campaign.go
+++ internal/models/entities/campaign.go
@@ -12,7 +12,7 @@ type Campaign struct {
 	Description     string    `json:"description" gorm:"type:text"`
 	GoalAmount      int       `json:"goal_amount" gorm:"type:integer;check:goal_amount>0"`
 	CollectedAmount int       `json:"collected_amount" gorm:"type:integer;default:0"`
-	Status          string    `json:"status" gorm:"type:varchar(20);default:'active'"`
+	IsActive        bool      `json:"is_active" gorm:"type:bool;default:true"`
 	CreatedAt       time.Time `json:"created_at" gorm:"type:timestamp;default:current_timestamp"`
 	UserId          uuid.UUID `json:"user_id" gorm:"type:uuid;not null"`
 }
--- internal/models/entities/user.go
+++ internal/models/entities/user.go
@@ -11,7 +11,7 @@ type User struct {
 	Name      string    `json:"name" gorm:"type:varchar(255)"`
 	Email     string    `json:"email" gorm:"type:varchar(255);unique"`
 	Password  string    `json:"password" gorm:"type:varchar(255)"`
-	Role      int       `json:"role" gorm:"type:smallint"`
+	IsAdmin   bool      `json:"is_admin" gorm:"type:bool;default:false"`
 	CreatedAt time.Time `json:"created_at" gorm:"type:timestamp;default:current_timestamp"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"type:timestamp;default:current_timestamp"`
 }
